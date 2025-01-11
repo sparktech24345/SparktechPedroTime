9959117e7d2b37a5dd29806a4ef1f81ae4163d85
DESKTOP-TBM70OS\Andrei <andrei.baloiu0@gmail.com>
2025-01-11 17:05:18 +0200

    Working Multitheead Imlementation  - Delta

---
+++ TeamCode/src/main/java/pedroPathing/tests/DriveOnlyTest.java
@@ -0,0 +1,130 @@
+package pedroPathing.tests;
+
+
+import static org.firstinspires.ftc.robotcore.external.BlocksOpModeCompanion.gamepad1;
+import static org.firstinspires.ftc.robotcore.external.BlocksOpModeCompanion.hardwareMap;
+import static org.firstinspires.ftc.robotcore.external.BlocksOpModeCompanion.linearOpMode;
+import static org.firstinspires.ftc.robotcore.external.BlocksOpModeCompanion.telemetry;
+import static pedroPathing.States.PositionStorage.backLeftPowerCat;
+import static pedroPathing.States.PositionStorage.backRightPowerCat;
+import static pedroPathing.States.PositionStorage.frontLeftPowerCat;
+import static pedroPathing.States.PositionStorage.frontRightPowerCat;
+import static pedroPathing.States.PositionStorage.intakeActualZero;
+import static pedroPathing.States.PositionStorage.intakeTargetPos;
+import static pedroPathing.States.PositionStorage.intakeTargetPosAdder;
+import static pedroPathing.States.PositionStorage.outakeTargetPos;
+
+import com.qualcomm.robotcore.eventloop.opmode.Disabled;
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.DcMotorSimple;
+import com.qualcomm.robotcore.hardware.Servo;
+
+import java.util.concurrent.Callable;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+
+import pedroPathing.ControlMotor;
+
+@TeleOp(name = "Only Drive Teleop", group = "Linear OpMode")
+
+public class DriveOnlyTest extends LinearOpMode {
+    ExecutorService executorService = Executors.newFixedThreadPool(2);
+
+    ControlMotor intakeControlMotor = new ControlMotor();
+
+    @Override
+    public void runOpMode() throws InterruptedException {
+
+       /* DcMotor frontLeftMotor = hardwareMap.dcMotor.get("frontleft");
+        DcMotor backLeftMotor = hardwareMap.dcMotor.get("backleft");
+        DcMotor frontRightMotor = hardwareMap.dcMotor.get("frontright");
+        DcMotor backRightMotor = hardwareMap.dcMotor.get("backright");
+        DcMotor intakeMotor = hardwareMap.dcMotor.get("intakemotor");
+
+        intakeMotor.setDirection(DcMotorSimple.Direction.REVERSE);
+
+        backLeftMotor.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+        frontLeftMotor.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+        backRightMotor.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+        frontRightMotor.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+        intakeMotor.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);*/
+
+
+        if (isStopRequested()) return;
+        waitForStart();
+
+        executorService.execute(new Runnable() {
+
+            @Override
+            public void run() {
+                DcMotor frontLeftMotor = hardwareMap.dcMotor.get("frontleft");
+                DcMotor backLeftMotor = hardwareMap.dcMotor.get("backleft");
+                DcMotor frontRightMotor = hardwareMap.dcMotor.get("frontright");
+                DcMotor backRightMotor = hardwareMap.dcMotor.get("backright");
+                DcMotor intakeMotor = hardwareMap.dcMotor.get("intakemotor");
+                intakeMotor.setDirection(DcMotorSimple.Direction.REVERSE);//*/
+
+                backLeftMotor.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+                frontLeftMotor.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+                backRightMotor.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+                frontRightMotor.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+                intakeMotor.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+                intakeMotor.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
+
+                while(opModeIsActive()){
+                    double intakeMotorPower = 0;
+                    intakeMotorPower = intakeControlMotor.PIDControl(intakeTargetPos+intakeActualZero+intakeTargetPosAdder, intakeMotor.getCurrentPosition());
+
+                    frontLeftMotor.setPower(frontLeftPowerCat);
+                    backLeftMotor.setPower(backLeftPowerCat);
+                    frontRightMotor.setPower(frontRightPowerCat);
+                    backRightMotor.setPower(backRightPowerCat);
+                    intakeMotor.setPower(intakeMotorPower);//*/
+                    telemetry.addLine("This is Motor "+Thread.currentThread().getId());
+                    updateTelemetry(telemetry);
+                }
+//*/
+            }
+        });
+        while (opModeIsActive()) {
+
+            // executorService.submit(Motors::new);
+            intakeTargetPos = 0;
+
+
+            ///gamepad1
+            double vertical = gamepad1.left_stick_y;
+            double horizontal = gamepad1.left_stick_x;
+            double pivot = gamepad1.right_stick_x;
+            boolean slowdown = gamepad1.left_bumper;
+
+            //calculating nedded power by method 1
+            frontRightPowerCat = (pivot - vertical - horizontal);
+            backRightPowerCat = (pivot - vertical + horizontal);
+            frontLeftPowerCat = (pivot + vertical - horizontal);
+            backLeftPowerCat = (pivot + vertical + horizontal);
+
+            //Pivot stuff
+            pivot = -pivot;
+
+
+            if(slowdown) {
+                frontRightPowerCat /= 3;
+                backRightPowerCat /= 3;
+                frontLeftPowerCat /= 3;
+                backLeftPowerCat /= 3;
+            }
+
+            telemetry.addData("frontLeftPower",frontLeftPowerCat);
+            telemetry.addData("backLeftPowerCat",backLeftPowerCat);
+            telemetry.addData("frontRightPowerCat",frontRightPowerCat);
+            telemetry.addData("backRightPowerCat",backRightPowerCat);
+            telemetry.addLine("This is Main "+Thread.currentThread().getId());
+            updateTelemetry(telemetry);
+
+        }
+
+    }
+}
--- TeamCode/src/main/java/pedroPathing/tests/EmptyTeleop.java
+++ TeamCode/src/main/java/pedroPathing/tests/EmptyTeleop.java
@@ -34,8 +34,7 @@ public class EmptyTeleop extends LinearOpMode {
 
     @Override
     public void runOpMode() throws InterruptedException {
-        ExecutorService executorService = Executors.newFixedThreadPool(4);
-        executorService.submit(Motors::new);
+
         updateTelemetry(telemetry);
         if (isStopRequested()) return;
 
@@ -47,65 +46,4 @@ public class EmptyTeleop extends LinearOpMode {
         }
 
     }
-}
-class Motors implements Callable<Object>{
-
-    double intakeMotorPower;
-    double outakeMotorPower;
-
-    ControlMotor intakeControlMotor;
-    ControlMotor outakeControlMotor;
-    private boolean DoOnce = true;
-    DcMotor frontLeftMotor = hardwareMap.dcMotor.get("frontleft");
-    DcMotor backLeftMotor = hardwareMap.dcMotor.get("backleft");
-    DcMotor frontRightMotor = hardwareMap.dcMotor.get("frontright");
-    DcMotor backRightMotor = hardwareMap.dcMotor.get("backright");
-    DcMotor intakeMotor = hardwareMap.dcMotor.get("intakemotor");
-    DcMotor outakeLeftMotor = hardwareMap.dcMotor.get("outakeleftmotor");
-    DcMotor outakeRightMotor = hardwareMap.dcMotor.get("outakerightmotor");
-
-    public Motors() {
-        intakeControlMotor = new ControlMotor();
-        outakeControlMotor = new ControlMotor();
-    }
-
-    @Override
-    public Object call() throws Exception {
-
-
-            intakeMotor.setDirection(DcMotorSimple.Direction.REVERSE);
-            outakeLeftMotor.setDirection(DcMotorSimple.Direction.REVERSE);
-
-            backLeftMotor.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
-            frontLeftMotor.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
-            backRightMotor.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
-            frontRightMotor.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
-            intakeMotor.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
-
-            intakeMotor.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
-            DoOnce = false;
-                frontLeftMotor.setPower(frontLeftPowerCat);
-                backLeftMotor.setPower(backLeftPowerCat);
-                frontRightMotor.setPower(frontRightPowerCat);
-                backRightMotor.setPower(backRightPowerCat);//*/
-                intakeMotorPower = intakeControlMotor.PIDControl(intakeTargetPos, intakeMotor.getCurrentPosition());
-                outakeMotorPower = outakeControlMotor.PIDControlUppy(outakeTargetPos, outakeRightMotor.getCurrentPosition());
-                intakeMotor.setPower(intakeMotorPower);
-                outakeRightMotor.setPower(outakeMotorPower);
-                outakeLeftMotor.setPower(outakeMotorPower);
-                telemetry.addData("Hello World", true);
-                telemetry.update();
-                Thread.sleep(15);
-
-            return null;
-    }
-
-}
-class Main implements Callable<Object>{
-
-    @Override
-    public Object call() throws Exception {
-        return null;
-    }
-
 }
