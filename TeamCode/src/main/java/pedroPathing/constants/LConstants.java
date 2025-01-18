9c1ffc1fb38b4fedd55541dd594b58571fb08528
DeltaHelios <stefan.v.iancu@gmail.com>
2025-01-18 16:04:51 +0200

    Deadwheel constants commenting

--- TeamCode/src/main/java/pedroPathing/constants/LConstants.java
+++ TeamCode/src/main/java/pedroPathing/constants/LConstants.java
@@ -9,18 +9,18 @@ import org.firstinspires.ftc.robotcore.external.navigation.DistanceUnit;
 
 public class LConstants {
     static {
-        ThreeWheelConstants.forwardTicksToInches = .001989436789;
-        ThreeWheelConstants.strafeTicksToInches = .001989436789;
-        ThreeWheelConstants.turnTicksToInches = .001989436789;
-        ThreeWheelConstants.leftY = 1;
-        ThreeWheelConstants.rightY = -1;
-        ThreeWheelConstants.strafeX = -2.5;
-        ThreeWheelConstants.leftEncoder_HardwareMapName = "leftFront";
-        ThreeWheelConstants.rightEncoder_HardwareMapName = "rightRear";
-        ThreeWheelConstants.strafeEncoder_HardwareMapName = "rightFront";
-        ThreeWheelConstants.leftEncoderDirection = Encoder.REVERSE;
-        ThreeWheelConstants.rightEncoderDirection = Encoder.REVERSE;
-        ThreeWheelConstants.strafeEncoderDirection = Encoder.FORWARD;
+//        ThreeWheelConstants.forwardTicksToInches = .001989436789;
+//        ThreeWheelConstants.strafeTicksToInches = .001989436789;
+//        ThreeWheelConstants.turnTicksToInches = .001989436789;
+//        ThreeWheelConstants.leftY = 1;
+//        ThreeWheelConstants.rightY = -1;
+//        ThreeWheelConstants.strafeX = -2.5;
+//        ThreeWheelConstants.leftEncoder_HardwareMapName = "leftFront";
+//        ThreeWheelConstants.rightEncoder_HardwareMapName = "rightRear";
+//        ThreeWheelConstants.strafeEncoder_HardwareMapName = "rightFront";
+//        ThreeWheelConstants.leftEncoderDirection = Encoder.REVERSE;
+//        ThreeWheelConstants.rightEncoderDirection = Encoder.REVERSE;
+//        ThreeWheelConstants.strafeEncoderDirection = Encoder.FORWARD;
 
         OTOSConstants.useCorrectedOTOSClass = true;
         OTOSConstants.hardwareMapName = "SparkFunSensor";
