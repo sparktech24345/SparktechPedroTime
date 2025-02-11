673617be3e028f845b384b470fd679a0136e82fc
DESKTOP-TBM70OS\Andrei <andrei.baloiu0@gmail.com>
2025-02-11 07:48:46 -1000

    more telemetrys

--- TeamCode/src/main/java/pedroPathing/tests/ConceptVisionColorLocatorTeleopTesting.java
+++ TeamCode/src/main/java/pedroPathing/tests/ConceptVisionColorLocatorTeleopTesting.java
@@ -234,6 +234,7 @@ public class ConceptVisionColorLocatorTeleopTesting extends LinearOpMode
                 RotatedRect boxFit = b.getBoxFit();
                 telemetry.addLine(String.format("%5d  %4.2f   %5.2f  (%3d,%3d)",
                           b.getContourArea(), b.getDensity(), b.getAspectRatio(), (int) boxFit.center.x, (int) boxFit.center.y));
+
                 dashboardTelemetry.addLine(String.format("%5d  %4.2f   %5.2f  (%3d,%3d)",
                           b.getContourArea(), b.getDensity(), b.getAspectRatio(), (int) boxFit.center.x, (int) boxFit.center.y));
             }
